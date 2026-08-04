c0f482cae52515f7b19fae8dd16aaa8c6716eb45
Souleymane DIALLO <sdley2020@gmail.com>
2026-08-04 16:30:42 +0000

    refactor OrderService to use constructor injection for PaymentService

--- src/main/java/sn/sdley/springbootstarter0/SpringBootStarter0Application.java
+++ src/main/java/sn/sdley/springbootstarter0/SpringBootStarter0Application.java
@@ -11,7 +11,7 @@ public class SpringBootStarter0Application {
 
     public static void main(String[] args) {
 //        SpringApplication.run(SpringBootStarter0Application.class, args);
-        var orderService = new OrderService();
+        var orderService = new OrderService(new PayPalPaymentService());
 //        orderService.setPaymentService(new PayPalPaymentService()); // uncomment this line to avoid NullPointerException in case
 //                                                                      // you switched to setter injection...
         orderService.placeOrder();
--- src/main/java/sn/sdley/springbootstarter0/service/OrderService.java
+++ src/main/java/sn/sdley/springbootstarter0/service/OrderService.java
@@ -1,18 +1,18 @@
 package sn.sdley.springbootstarter0.service;
 
 public class OrderService {
-    private PaymentService paymentService;
+    private final PaymentService paymentService;
 
-//    public OrderService(PaymentService paymentService) {
-//        this.paymentService = paymentService;
-//    }
+    public OrderService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
 
     public void placeOrder() {
         System.out.println("Placing order...");
         paymentService.processPayment(100.0);
     }
 
-    public void setPaymentService(PaymentService paymentService) {
-        this.paymentService = paymentService;
-    }
+//    public void setPaymentService(PaymentService paymentService) {
+//        this.paymentService = paymentService;
+//    }
 }
