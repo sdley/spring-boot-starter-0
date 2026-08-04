0fb9073eff82c0aef251a50844abf1b464747ecf
Souleymane DIALLO <sdley2020@gmail.com>
2026-08-04 12:52:34 +0000

    add spring-boot-devtools dependency and enhance tests for index.html serving

--- src/test/java/sn/sdley/springbootstarter0/SpringBootStarter0ApplicationTests.java
+++ src/test/java/sn/sdley/springbootstarter0/SpringBootStarter0ApplicationTests.java
@@ -1,13 +1,40 @@
 package sn.sdley.springbootstarter0;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @SpringBootTest
 class SpringBootStarter0ApplicationTests {
 
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = webAppContextSetup(webApplicationContext).build();
+    }
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void indexHtmlIsServedAsStaticContent() throws Exception {
+        mockMvc.perform(get("/index.html"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Welcome to the Home Page")));
     }
 
 }
