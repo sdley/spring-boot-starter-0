ec6f84f8993e76d7479ad7d4699ef51ef7cb333e
Souleymane DIALLO <sdley2020@gmail.com>
2026-08-04 12:32:13 +0000

    initialize Spring Boot project with Maven wrapper and basic structure

---
+++ src/main/java/sn/sdley/springbootstarter0/SpringBootStarter0Application.java
@@ -0,0 +1,13 @@
+package sn.sdley.springbootstarter0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarter0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStarter0Application.class, args);
+    }
+
+}
