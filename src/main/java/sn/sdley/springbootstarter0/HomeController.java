32066e08d61325be3e6eb5b4ddee54ef4f7868bb
Souleymane DIALLO <sdley2020@gmail.com>
2026-08-04 12:44:06 +0000

    add HomeController and index.html for home page routing

---
+++ src/main/java/sn/sdley/springbootstarter0/HomeController.java
@@ -0,0 +1,13 @@
+package sn.sdley.springbootstarter0;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index.html";
+    }
+}
