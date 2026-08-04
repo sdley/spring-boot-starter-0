43730b78019b3c59e5be1e4e348b426cf153c4cb
Souleymane DIALLO <sdley2020@gmail.com>
2026-08-04 15:47:45 +0000

    inject PaymentService dependency into OrderService for improved payment processing

--- src/main/java/sn/sdley/springbootstarter0/service/OrderService.java
+++ src/main/java/sn/sdley/springbootstarter0/service/OrderService.java
@@ -1,10 +1,14 @@
 package sn.sdley.springbootstarter0.service;
 
 public class OrderService {
+    private final PaymentService paymentService;
+
+    public OrderService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
 
     public void placeOrder() {
         System.out.println("Placing order...");
-        var paymentService = new StripePaymentService();
         paymentService.processPayment(100.0);
     }
 }
