c8e5061a3d840a319770be8a88f041269a8626aa
Souleymane DIALLO <sdley2020@gmail.com>
2026-09-06 19:06:15 +0000

    refactor HomeController to use AppProperties for configuration management

--- src/main/java/sn/sdley/springbootstarter0/controller/HomeController.java
+++ src/main/java/sn/sdley/springbootstarter0/controller/HomeController.java
@@ -3,19 +3,24 @@ package sn.sdley.springbootstarter0.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import sn.sdley.springbootstarter0.config.AppProperties;
 
 @Controller
 class HomeController {
 
-    @Value("${spring.application.name}")
-    private String appName;
-    @Value("${app.page-size}")
-    private int appPage;
+    private final String appName;
+    private final AppProperties appProperties;
+
+    HomeController(@Value("${spring.application.name}") String appName,
+                   AppProperties appProperties) {
+        this.appName = appName;
+        this.appProperties = appProperties;
+    }
 
     @GetMapping("/")
     String index() {
         System.out.println("Application Name: " + appName);
-        System.out.println("Application Page Size: " + appPage);
+        System.out.println("Application Page Size: " + appProperties.getPage().getSize());
         return "forward:/index.html";
     }
 }
