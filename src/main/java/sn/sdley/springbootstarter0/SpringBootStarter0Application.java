793c4b783f2ade9485a6b33018baeb15491fab4d
Souleymane DIALLO <sdley2020@gmail.com>
2026-09-07 11:31:50 +0000

    add User entity and integrate Lombok for boilerplate reduction

--- src/main/java/sn/sdley/springbootstarter0/SpringBootStarter0Application.java
+++ src/main/java/sn/sdley/springbootstarter0/SpringBootStarter0Application.java
@@ -3,6 +3,7 @@ package sn.sdley.springbootstarter0;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import sn.sdley.springbootstarter0.entities.User;
 import sn.sdley.springbootstarter0.service.OrderService;
 import sn.sdley.springbootstarter0.service.PayPalPaymentService;
 import sn.sdley.springbootstarter0.service.StripePaymentService;
@@ -11,9 +12,14 @@ import sn.sdley.springbootstarter0.service.StripePaymentService;
 public class SpringBootStarter0Application {
 
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(SpringBootStarter0Application.class, args);
-        var orderService = context.getBean(OrderService.class);
-        orderService.placeOrder();
+//        ApplicationContext context = SpringApplication.run(SpringBootStarter0Application.class, args);
+        // Creating a new with builder pattern
+        User user = User.builder()
+                .name("John Doe")
+                .email("john.doe@example.com")
+                .password("password123")
+                .build();
+
     }
 
 }
---
+++ src/main/java/sn/sdley/springbootstarter0/entities/User.java
@@ -0,0 +1,24 @@
+package sn.sdley.springbootstarter0.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, name = "name")
+    private String name;
+    @Column(nullable = false, name = "email")
+    private String email;
+    @Column(nullable = false, name = "password")
+    private String password;
+
+}
