0199070bbbc9170b61f09f69806455848bbbad6d
Souleymane DIALLO <sdley2020@gmail.com>
2026-09-11 15:57:11 +0000

    add ToString annotation to Product class for improved string representation

--- src/main/java/sn/sdley/springbootstarter0/entities/Product.java
+++ src/main/java/sn/sdley/springbootstarter0/entities/Product.java
@@ -3,11 +3,13 @@ package sn.sdley.springbootstarter0.entities;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.math.BigDecimal;
 
 @Getter
 @Setter
+@ToString
 @Entity
 @Table(name = "products")
 public class Product {
