77d98640d1865743f6569284c790d2bda7ead042
Souleymane DIALLO <sdley2020@gmail.com>
2026-09-12 16:25:27 +0000

    add UserController; implement endpoint to retrieve user by ID

--- src/main/java/sn/sdley/springbootstarter0/controller/UserController.java
+++ src/main/java/sn/sdley/springbootstarter0/controller/UserController.java
@@ -2,18 +2,26 @@ package sn.sdley.springbootstarter0.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sn.sdley.springbootstarter0.entities.User;
 import sn.sdley.springbootstarter0.repositories.UserRepository;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/users")
 public class UserController {
 
     private final UserRepository userRepository;
 
-    @GetMapping("/users")
+    @GetMapping
     public Iterable<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id).orElse(null);
     }
 }
