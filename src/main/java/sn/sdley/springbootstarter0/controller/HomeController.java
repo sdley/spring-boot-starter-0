313ecdcf8bc1d3c01caa60eedea41865692ca2ba
Souleymane DIALLO <sdley2020@gmail.com>
2026-09-12 12:39:42 +0000

    add HomeController and Thymeleaf dependency; implement index method for dynamic greeting

---
+++ src/main/java/sn/sdley/springbootstarter0/controller/HomeController.java
@@ -0,0 +1,15 @@
+package sn.sdley.springbootstarter0.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("name", "Souleymane");
+        return "index";
+    }
+}
