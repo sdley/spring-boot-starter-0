a41ac9990ba4c911da6bfd94586927332b7b75a8
Souleymane DIALLO <sdley2020@gmail.com>
2026-09-13 23:05:11 +0000

    update UserController; enhance getUserById method to return ResponseEntity with proper handling for non-existent users

--- src/main/java/sn/sdley/springbootstarter0/controller/UserController.java
+++ src/main/java/sn/sdley/springbootstarter0/controller/UserController.java
@@ -1,6 +1,7 @@
 package sn.sdley.springbootstarter0.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,12 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
-    public User getUserById(@PathVariable Long id) {
-        return userRepository.findById(id).orElse(null);
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        var user = userRepository.findById(id).orElse(null);
+
+        if  (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(user);
     }
 }
