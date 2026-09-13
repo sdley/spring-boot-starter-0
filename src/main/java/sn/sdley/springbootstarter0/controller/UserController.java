7e4878503281a170fe54946f7649b175c5a2bb43
Souleymane DIALLO <sdley2020@gmail.com>
2026-09-13 23:20:58 +0000

    update UserController; modify getAllUsers and getUserById methods to return UserDto objects and switch UserRepository to JpaRepository

--- src/main/java/sn/sdley/springbootstarter0/controller/UserController.java
+++ src/main/java/sn/sdley/springbootstarter0/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sn.sdley.springbootstarter0.dtos.UserDto;
 import sn.sdley.springbootstarter0.entities.User;
 import sn.sdley.springbootstarter0.repositories.UserRepository;
 
@@ -17,17 +18,17 @@ public class UserController {
     private final UserRepository userRepository;
 
     @GetMapping
-    public Iterable<User> getAllUsers() {
-        return userRepository.findAll();
+    public Iterable<UserDto> getAllUsers() {
+        return userRepository.findAll().stream().map(user -> new UserDto(user.getId(), user.getName(), user.getEmail())).toList();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long id) {
         var user = userRepository.findById(id).orElse(null);
 
         if  (user == null) {
             return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.ok(user);
+        return ResponseEntity.ok(new UserDto(user.getId(), user.getName(), user.getEmail()));
     }
 }
--- src/main/java/sn/sdley/springbootstarter0/repositories/UserRepository.java
+++ src/main/java/sn/sdley/springbootstarter0/repositories/UserRepository.java
@@ -1,8 +1,9 @@
 package sn.sdley.springbootstarter0.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import sn.sdley.springbootstarter0.entities.User;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
