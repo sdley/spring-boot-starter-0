7b2c9fc01172d5076c58dd65876e5d17cf3dc3cb
Souleymane DIALLO <sdley2020@gmail.com>
2026-09-14 11:03:39 +0000

    update UserDto and UserMapper; add createdAt field with JSON format and mapping expression for UserDto

--- src/main/java/sn/sdley/springbootstarter0/dtos/UserDto.java
+++ src/main/java/sn/sdley/springbootstarter0/dtos/UserDto.java
@@ -1,7 +1,10 @@
 package sn.sdley.springbootstarter0.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @Getter
@@ -9,4 +12,6 @@ public class UserDto {
     private Long id;
     private String name;
     private String email;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
 }
--- src/main/java/sn/sdley/springbootstarter0/mappers/UserMapper.java
+++ src/main/java/sn/sdley/springbootstarter0/mappers/UserMapper.java
@@ -1,10 +1,12 @@
 package sn.sdley.springbootstarter0.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import sn.sdley.springbootstarter0.dtos.UserDto;
 import sn.sdley.springbootstarter0.entities.User;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    @Mapping(target="createdAt", expression = "java(java.time.LocalDateTime.now())")
     UserDto toDto(User user);
 }
