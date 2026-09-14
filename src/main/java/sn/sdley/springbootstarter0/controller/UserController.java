e89a24015ae5170aebd56af55e4b61b1e070836b
Souleymane DIALLO <sdley2020@gmail.com>
2026-09-14 11:50:30 +0000

    update UserController; add sorting functionality to getAllUsers method and remove profile association from User

--- src/main/java/sn/sdley/springbootstarter0/controller/UserController.java
+++ src/main/java/sn/sdley/springbootstarter0/controller/UserController.java
@@ -1,14 +1,14 @@
 package sn.sdley.springbootstarter0.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sn.sdley.springbootstarter0.dtos.UserDto;
 import sn.sdley.springbootstarter0.mappers.UserMapper;
 import sn.sdley.springbootstarter0.repositories.UserRepository;
+
+import java.util.Set;
 
 @RestController
 @AllArgsConstructor
@@ -19,8 +19,14 @@ public class UserController {
     private final UserMapper userMapper;
 
     @GetMapping
-    public Iterable<UserDto> getAllUsers() {
-        return userRepository.findAll()
+    public Iterable<UserDto> getAllUsers(
+            @RequestParam (required = false, defaultValue = "", name = "sort") String sortBy
+    ) {
+        if (!Set.of("name", "email").contains(sortBy)) {
+            sortBy = "id";
+        }
+
+        return userRepository.findAll(Sort.by(sortBy))
                 .stream()
                 // .map(user -> userMapper.toDto(user))
                 .map(userMapper::toDto)
