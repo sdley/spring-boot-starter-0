254b36b7cf87d0cff064b02ac133349cfa955c70
Souleymane DIALLO <sdley2020@gmail.com>
2026-09-17 11:47:24 +0000

    add GlobalExceptionHandler to manage validation errors globally in the application

---
+++ src/main/java/sn/sdley/springbootstarter0/controller/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package sn.sdley.springbootstarter0.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(
+            MethodArgumentNotValidException exception
+    ) {
+        var errors = new HashMap<String, String>();
+
+        exception.getBindingResult().getFieldErrors()
+                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+}
