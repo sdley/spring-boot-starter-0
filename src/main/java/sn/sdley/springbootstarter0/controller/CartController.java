f4a2736e198038491e7dda41d1e54652f3fd47f3
Souleymane DIALLO <sdley2020@gmail.com>
2026-09-19 13:47:03 +0000

    add functionality to add items to cart, including AddItemToCartRequest DTO, CartItem total price calculation, and updates to CartController and CartMapper

--- src/main/java/sn/sdley/springbootstarter0/controller/CartController.java
+++ src/main/java/sn/sdley/springbootstarter0/controller/CartController.java
@@ -3,14 +3,18 @@ package sn.sdley.springbootstarter0.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
+import sn.sdley.springbootstarter0.dtos.AddItemToCartRequest;
 import sn.sdley.springbootstarter0.dtos.CartDto;
+import sn.sdley.springbootstarter0.dtos.CartItemDto;
 import sn.sdley.springbootstarter0.entities.Cart;
+import sn.sdley.springbootstarter0.entities.CartItem;
 import sn.sdley.springbootstarter0.mappers.CartMapper;
 import sn.sdley.springbootstarter0.repositories.CartRepository;
+import sn.sdley.springbootstarter0.repositories.ProductRepository;
+
+import java.util.UUID;
 
 @AllArgsConstructor
 @RestController
@@ -19,6 +23,7 @@ public class CartController {
 
     private final CartRepository cartRepository;
     private final CartMapper cartMapper;
+    private final ProductRepository productRepository;
 
     @PostMapping
     public ResponseEntity<CartDto> createCart(
@@ -31,5 +36,42 @@ public class CartController {
         var uri = uriComponentsBuilder.path("/carts/{id}").buildAndExpand(cart.getId()).toUri();
 
         return ResponseEntity.created(uri).body(cartDto);
+    }
+
+    @PostMapping("/{cartId}/items")
+    public ResponseEntity<CartItemDto> addItemToCart(
+            @PathVariable UUID cartId,
+            @RequestBody AddItemToCartRequest request
+    ){
+        var cart = cartRepository.findById(cartId).orElse(null);
+        if (cart == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        var product = productRepository.findById(request.getProductId()).orElse(null);
+        if (product == null){
+            return ResponseEntity.badRequest().build();
+        }
+
+        var cartItem = cart.getCartItems().stream()
+                .filter(item -> item.getProduct().getId().equals(product.getId()))
+                .findFirst()
+                .orElse(null);
+
+        if (cartItem == null) {
+            cartItem = new CartItem();
+            cartItem.setCart(cart);
+            cartItem.setProduct(product);
+            cartItem.setQuantity(1);
+            cart.getCartItems().add(cartItem);
+        } else {
+            cartItem.setQuantity(cartItem.getQuantity() + 1);
+        }
+
+        cartRepository.save(cart);
+
+        var cartItemDto = cartMapper.toDto(cartItem);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(cartItemDto);
     }
 }
--- src/main/java/sn/sdley/springbootstarter0/mappers/CartMapper.java
+++ src/main/java/sn/sdley/springbootstarter0/mappers/CartMapper.java
@@ -1,10 +1,16 @@
 package sn.sdley.springbootstarter0.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import sn.sdley.springbootstarter0.dtos.CartDto;
+import sn.sdley.springbootstarter0.dtos.CartItemDto;
 import sn.sdley.springbootstarter0.entities.Cart;
+import sn.sdley.springbootstarter0.entities.CartItem;
 
 @Mapper(componentModel = "spring")
 public interface CartMapper {
     CartDto toDto(Cart cart);
+
+    @Mapping(target = "totalPrice", expression = "java(cartItem.getTotalPrice())")
+    CartItemDto toDto(CartItem cartItem);
 }
