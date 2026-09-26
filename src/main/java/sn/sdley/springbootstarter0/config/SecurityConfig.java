30c8aeb2e4c74e00ef3b2c4468b0f49669cc96b5
Souleymane DIALLO <sdley2020@gmail.com>
2026-09-26 13:46:37 +0000

    add AuthController for user authentication and implement login functionality with password validation

--- src/main/java/sn/sdley/springbootstarter0/config/SecurityConfig.java
+++ src/main/java/sn/sdley/springbootstarter0/config/SecurityConfig.java
@@ -23,6 +23,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/carts/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .anyRequest().authenticated());
         return http.build();
     }
--- src/main/java/sn/sdley/springbootstarter0/repositories/UserRepository.java
+++ src/main/java/sn/sdley/springbootstarter0/repositories/UserRepository.java
@@ -5,7 +5,12 @@ import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sn.sdley.springbootstarter0.entities.User;
 
+import javax.imageio.spi.ServiceRegistry;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(@NotBlank(message = "Email is required") @Email(message = "Email should be valid") String email);
+
+    Optional<User> findByEmail(@NotBlank(message = "Email is required") @Email(message = "Email should be valid") String email);
 }
